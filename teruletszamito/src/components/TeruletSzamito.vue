fc9c30dc396b42e3a418f4316954065b699c0ece
Tamas Dukai <tomi.2@hotmail.co.uk>
2024-05-08 13:26:37 +0200

    területszámítás vue

---
+++ teruletszamito/src/components/TeruletSzamito.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h2>Területszámító</h2>
+    <br />
+
+    <label for="inputA">A értéke:</label>
+    <input v-model="a" type="number" placeholder="Adja meg az A értékét" />
+
+    <br />
+    <br />
+
+    <label for="inputB">B értéke:</label>
+    <input v-model="b" type="number" placeholder="Adja meg a B értékét" />
+
+    <br />
+    <br />
+
+    <button @click="szamolTerulet">Számol</button>
+
+    <br />
+    <br />
+
+    <p>{{ eredmeny }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: "",
+      b: "",
+      eredmeny: "",
+    };
+  },
+  methods: {
+    szamolTerulet() {
+      if (!this.a || !this.b) {
+        alert("Kérem töltse ki mindkét mezőt!");
+        return;
+      }
+
+      this.eredmeny = "A terület: " + this.a * this.b;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Itt lehet stílusokat megadni, ha szükséges */
+</style>
